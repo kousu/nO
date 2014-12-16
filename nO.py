4cac490ebe90ff6dd302a876ae0e9818f0887eea
kousu <nick@kousu.ca>
2014-12-15 23:15:09 -0500

    make lazy, so that longish counts are feasible

--- nO.py
+++ nO.py
@@ -6,9 +6,6 @@ catherine: No No No
 catherine: how do i write a program so i can have all the character differences for no no no
 """
 
-# one solution: itertools.combinations(itertools.combinations(("N", "n")), itertools.combinations(("O, "o"))) then chained to give three at a time
-# two solution: write a function that takes a string, finds all the alpha chars, and walks the bitstrings gives the 2**(# alpha char) dif
-
 import sys
 from itertools import product
 import argparse
@@ -18,7 +15,7 @@ def scream(word, count=3):
 	crazy = product(*word) #all the ways of writing 'word'
 	crazy = [str.join("", word) for word in crazy]    #pump the generator and flatten the letters back, since we want to reuse it
 	many = product(*([crazy]*count)) #pick out 'count' crazed words
-	many = [str.join(" ", shout) for shout in many] #ditto, but instead we're flattening words->sentence instead of letters->word
+	many = (str.join(" ", shout) for shout in many) #ditto, but instead we're flattening words->sentence instead of letters->word
 	return many
 
 
